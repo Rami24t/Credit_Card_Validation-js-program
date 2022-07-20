b28daaa49b104e1a286d607fb3c00cd2cf9aaf3c
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-20 22:21:30 +0200

    solution+bonus1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function validateCreditCard(creditCardNum) {
 // returns  `true`  or  `false`
-n = removeDashes(n);
+creditCardNum = removeDashes(creditCardNum);
 return is16digits(creditCardNum) && isNumber(creditCardNum) && includesDifferent(creditCardNum) && isLastEven(creditCardNum)  && isSumGT16(creditCardNum);
 };
 
@@ -90,9 +90,9 @@ else
 
 
 
-// /**** tests *****/
-// console.log(validateCreditCard('9999-7777-8888-0000')); //{ valid: true, number: '9999-7777-8888-0000' }
-// console.log(validateCreditCard('6666-6666-6666-1666')); //{ valid: true, number: '6666-6666-6666-1666' }
+// /**** tests Part 2: Bonus Parts *****/
+console.log(validateCreditCard('9999-7777-8888-0000')); //{ valid: true, number: '9999-7777-8888-0000' }
+console.log(validateCreditCard('6666-6666-6666-1666')); //{ valid: true, number: '6666-6666-6666-1666' }
 // console.log(validateCreditCard('a923-3211-9c01-1112')); //{ valid: false,number: 'a923-3211-9c01-1112',error: '_invalid characters_' }
 // console.log(validateCreditCard('4444-4444-4444-4444')); //{ valid: false,number: '4444-4444-4444-4444',error: '_only one type of number_' }
-// console.log(validateCreditCard('1211-1111-1111-1112')); //{ valid: true, number: '1211-1111-1111-1112' }
+console.log(validateCreditCard('1211-1111-1111-1112')); //{ valid: true, number: '1211-1111-1111-1112' }
